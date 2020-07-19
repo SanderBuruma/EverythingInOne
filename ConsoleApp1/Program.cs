be6a4fc829bb7ca4bf78d790dc109fec4a500cf8
Sander Buruma <sanderburuma@gmail.com>
2020-07-19 22:58:16 +0200

    more logic implemented and a little more detailed.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Text;
 using System.Text.RegularExpressions;
 
-namespace ConsoleApp1
+namespace KerbalTradingProgram
 {
     class Program
     {
@@ -14,107 +15,175 @@ namespace ConsoleApp1
     #region ship
     class Ship
     {
-        /// <summary>(x,y)In meters compared to starting position, right and up are positive</summary>
-        private (Double, Double) _position;
-        private Double _dryMassKg;
-        private Double _fuelMassKg;
-        private Double _exhVelocityMs;
-        private Double _thrustKn;
-        private (Double, Double) _velocity;
+        #region Fields
+        /// <summary>(x,y) In meters compared to starting position, right and up are positive</summary>
+        private (double, double) _position;
+        private double _dryMassKg;
+        private double _fuelMassKg;
+        private double _exhVelocityMs;
+        private double _thrustN;
+        private (double, double) _velocity;
         private ShipStatus _shipStatus;
-        private Planet _planet;
+        private Planet _currentPlanet;
         private Ship _secondStage;
-        private Int32 _fraction;
-        private Int32 _ticksInFlight;
+        private int _fraction;
+        private int _ticksInFlight;
+        ///<summary>0 is eastbound, 180 is west; 270 isn't north, it's up.</summary>
+        private double _rotation;
+        /// <summary>rad/s</summary>
+        private double _angularMomentum;
+        #endregion
 
+        #region Constructor
         public Ship(
-            (Double, Double) position,
-            (Double, Double) velocity,
+            (double, double) position,
+            (double, double) velocity,
             double dryMassKg,
             double fuelMassKg,
             double exhVelocityMs,
-            double thrustKn,
-            Planet planet,
-            Ship secondStage = null
+            double thrustN,
+            Planet currentPlanet,
+            Ship secondStage = null,
+            int fraction = 30,
+            double rotation = 270,
+            double angularMomentum = 0
         )
         {
-            if (dryMassKg <= 0 )
+            if (dryMassKg <= 0)
                 throw new Exception(String.Format("{0} param was 0 or less and shouldn't have been", nameof(dryMassKg)));
-            if (fuelMassKg < 0 )
+            if (fuelMassKg < 0)
                 throw new Exception(String.Format("{0} less than 0 and shouldn't have been", nameof(fuelMassKg)));
-            if (thrustKn < 0 )
-                throw new Exception(String.Format("{0} less than 0 and less and shouldn't have been", nameof(thrustKn)));
-            if (velocity.Item2 < 0)
-                throw new Exception(String.Format("{0} less than 0 and less and shouldn't have been", nameof(velocity) + "item2"));
+            if (thrustN < 0)
+                throw new Exception(String.Format("{0} less than 0 and less and shouldn't have been", nameof(thrustN)));
+            if (position.Item2 < 0)
+                throw new Exception(String.Format("{0} less than 0 and less and shouldn't have been", nameof(position) + ".Item2"));
             if (exhVelocityMs <= 0)
                 throw new Exception(String.Format("{0} param was 0 or less and shouldn't have been", nameof(exhVelocityMs)));
+            if (fraction <= 0)
+                throw new Exception(String.Format("{0} param was 0 or less and shouldn't have been", nameof(fraction)));
 
             _position = position;
             _dryMassKg = dryMassKg;
             _fuelMassKg = fuelMassKg;
             _exhVelocityMs = exhVelocityMs;
-            _thrustKn = thrustKn;
+            _thrustN = thrustN;
             _velocity = velocity;
             _shipStatus = ShipStatus.Landed;
-            _planet = planet;
+            _currentPlanet = currentPlanet;
             _secondStage = secondStage;
-            _fraction = 30;
+            _fraction = fraction;
             _ticksInFlight = 0;
+
+            rotation = rotation / (360 / (2 * Math.PI));
+            _rotation = rotation % (2 * Math.PI);
+            _angularMomentum = angularMomentum % (2 * Math.PI);
+        }
+        #endregion
+
+        #region Properties
+        public double DryMass               { get { return _dryMassKg; } }
+        public double ExhaustVelocity       { get { return _exhVelocityMs; } }
+        public double FuelMass              { get { return _fuelMassKg; }   set { _fuelMassKg = value; } }
+        public Planet Planet                { get { return _currentPlanet; } }
+        public (double, double) Position    { get { return _position; }     set { _position = value; } }
+        public Ship SecondStage             { get { return _secondStage; } }
+        public double Thrust                { get { return _thrustN; } }
+        public int TicksInFlight            { get => _ticksInFlight;        set => _ticksInFlight = value; }
+        public (double, double) Velocity    { get { return _velocity; }     set { _velocity = value; } }
+
+        public double Acceleration { get { return Thrust / TotalMass; } }
+
+        //local gravity minus centripetal acceleration
+        public double EffectiveGravity
+        {
+            get
+            {
+                return LocalGravity - Velocity.Item1 * Velocity.Item1 / Planet.Radius;
+            }
         }
 
-        public Double FuelMass { get { return _fuelMassKg; } }
-        public Double DryMass { get { return _dryMassKg; } }
-        public (Double, Double) Velocity { get { return _velocity; } }
-        public (Double, Double) Position { get { return _position; } }
-        public Double Acceleration { get { return _thrustKn / TotalMass; } }
-        public Double TWR { get { return Acceleration / LocalGravity; } }
+        public double FuelUse { get { return _thrustN / _exhVelocityMs; } }
+
+        public double LocalGravity
+        {
+            get
+            {
+                return Math.Pow(
+                    _currentPlanet.Radius / (_currentPlanet.Radius + _position.Item2)
+                    , 2)
+                * _currentPlanet.SurfaceGravity;
+            }
+        }
+
+        public double Rotation
+        {
+            get { return _rotation; }
+            set { _rotation = value % (2 * Math.PI); }
+        }
+        public double AngularMomentum
+        {
+            get { return _angularMomentum; }
+            set { _angularMomentum = value % (2 * Math.PI); }
+        }
 
         public ShipStatus Status {
             get { return _shipStatus; }
             set { _shipStatus = value; }
         }
-        public Double TotalMass { get {
-            if (_secondStage == null) return _fuelMassKg + _dryMassKg;
-            return _fuelMassKg + _dryMassKg + _secondStage.TotalMass;
-        } }
-        public Double LocalGravity
+
+        public double TotalMass
         {
             get {
-                return Math.Pow(
-                    _planet.Radius / (_planet.Radius + _position.Item2)
-                    , 2)
-                * _planet.SurfaceGravity;
+                if (_secondStage == null) return _fuelMassKg + _dryMassKg;
+                return _fuelMassKg + _dryMassKg + _secondStage.TotalMass;
             }
         }
 
+        public double TWR { get { return Acceleration / EffectiveGravity; } }
+        #endregion
+
         #region Methods
         //ticks the ship a fraction of a second ahead (1/fraction)
-        public ShipStatus Tick(Int32 fraction)
+        public ShipStatus Tick(int fraction, bool dontRunEngines = false)
         {
-            _position = (
+            TicksInFlight++;
+            if (SecondStage != null) SecondStage.Tick(fraction, true);
+
+            Position = (
                 Position.Item1 + Velocity.Item1/fraction,
                 Position.Item2 + Velocity.Item2/fraction
             );
 
-            if (_position.Item1 < 0) {
+            if (Position.Item1 < 0) {
                 if (Status == ShipStatus.Crashed) throw new Exception("Ship already crashed, no further ticks should have taken place");
                 Status = ShipStatus.Crashed;
             }
 
             //if no fuel don't do anything else
-            if (_fuelMassKg <= 0) return Status;
-            _fuelMassKg -= _thrustKn / _exhVelocityMs;
+            (double, double) thrustAcceleration = (0, 0);
+            if (FuelMass > 0 && !dontRunEngines)
+            {
+                //todo implement proper ship controls and related velocity changes.
+                thrustAcceleration.Item1 += Math.Sqrt(Acceleration*Acceleration - LocalGravity*LocalGravity);
+            }
+            else Status = ShipStatus.OutOfFuel;
+
+            FuelMass -= FuelUse / fraction;
 
             //change velocity
+            Velocity = (
+                Velocity.Item1 + thrustAcceleration.Item1,
+                Velocity.Item2 + thrustAcceleration.Item2 - LocalGravity);
 
             return Status;
         }
         #endregion
 
         public enum ShipStatus {
+            Crashed,
+            OutOfFuel,
             Landed,
             Flying,
-            Crashed,
             Orbiting
         }
     }
@@ -123,8 +192,8 @@ namespace ConsoleApp1
     #region planet
     class Planet
     {
-        private Double _surfaceGravitym2s;
-        private Double _radiusInM;
+        private double _surfaceGravitym2s;
+        private double _radiusInM;
 
         public Planet(double surfaceGravitym2s, double radiusInM)
         {
@@ -132,8 +201,8 @@ namespace ConsoleApp1
             _radiusInM = radiusInM;
         }
 
-        public Double SurfaceGravity { get { return _surfaceGravitym2s;} }
-        public Double Radius { get { return _radiusInM;} }
+        public double SurfaceGravity { get { return _surfaceGravitym2s;} }
+        public double Radius { get { return _radiusInM;} }
     }
     #endregion
 }
