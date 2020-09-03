9e2e5c70a81372d43ee3ac1c08075bbe7549d757
Sander Buruma <sanderburuma@gmail.com>
2020-09-03 09:17:59 +0200

    more updates

--- PersonalWebsite/ClientApp/src/app/app.module.ts
+++ PersonalWebsite/ClientApp/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule }           from '@angular/platform-browser';
+import { NgModule }                from '@angular/core';
+import { FormsModule }             from '@angular/forms';
+import { HttpClientModule }        from '@angular/common/http';
+import { RouterModule }            from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AppComponent }            from './app.component';
+import { NavMenuComponent }        from './nav-menu/nav-menu.component';
+import { HomeComponent }           from './home/home.component';
+import { CounterComponent }        from './counter/counter.component';
+import { FetchDataComponent }      from './fetch-data/fetch-data.component';
 
-import { HttpService } from './shared/services/Http.service';
-import { ThemesService } from './shared/services/Themes.service';
+import { MatCheckboxModule }       from '@angular/material/checkbox';
+import { MatButtonModule }         from '@angular/material/button';
+import { MatToolbarModule }        from '@angular/material/toolbar';
+import { MatCardModule }           from '@angular/material/card';
+import { MatDividerModule }        from '@angular/material/divider';
+import { MatIconModule }           from '@angular/material/icon';
+import { MatProgressBarModule }    from '@angular/material/progress-bar';
+import { MatSnackBarModule }       from '@angular/material/snack-bar';
+import { OverlayModule }           from '@angular/cdk/overlay';
+
+import { HttpService }             from './shared/services/Http.service';
+import { ThemesService }           from './shared/services/Themes.service';
 
 @NgModule({
   declarations: [
@@ -31,6 +41,16 @@ import { ThemesService } from './shared/services/Themes.service';
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ]),
+    BrowserAnimationsModule,
+    OverlayModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
     BrowserAnimationsModule
   ],
   providers: [HttpService, ThemesService],
---
+++ PersonalWebsite/readme.md
@@ -0,0 +1,3 @@
+# plans for the future:
+- Database
+- Localization (with help of database, containing Id(pk), LanguageId(fk), GroupId(fk), XmlLabel, Value)
