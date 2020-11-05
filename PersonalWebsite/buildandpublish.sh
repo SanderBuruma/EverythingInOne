db3b6ef76914042a71d1b5aaddd0b1257d95e108
Sander Buruma <sanderburuma@fastmail.nl>
2020-11-05 20:19:02 +0100

    1. Updated build and publish .sh
    2. title attributes now get dynamically updated
    3. Latin localization tweaked

--- PersonalWebsite/buildandpublish.sh
+++ PersonalWebsite/buildandpublish.sh
@@ -1,3 +1,4 @@
 sudo docker login
-sudo docker build -t sanderburuma/personal-website .
+sudo docker image prune --all --force
+sudo docker build -t sanderburuma/personal-website:latest .
 sudo docker push sanderburuma/personal-website
